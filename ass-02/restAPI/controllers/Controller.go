872ca00537c0965bc0bc15c7d55f724f42a8a4d9
Elang-Eka <elangputra87@gmail.com>
2022-10-07 14:35:01 +0700

    Update

--- ass-02/restAPI/controllers/Controller.go
+++ ass-02/restAPI/controllers/Controller.go
@@ -1,5 +1,6 @@
 package controllers
 
+// import package
 import (
 	"net/http"
 	"restapi/structs"
@@ -9,6 +10,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// struct data
 type Data struct {
 	ItemCode    string `json:"item_code"`
 	Description string `json:"description"`
@@ -16,7 +18,8 @@ type Data struct {
 }
 
 func (idb *InDB) CreateOrders(c *gin.Context) {
-	// declare variabel
+
+	// init variabel
 	var (
 		order  structs.Order
 		item   structs.Item
@@ -67,7 +70,8 @@ func (idb *InDB) CreateOrders(c *gin.Context) {
 			}
 			c.JSON(http.StatusNoContent, result)
 		} else {
-			// response
+
+			// response success
 			c.JSON(http.StatusOK, gin.H{
 				"orderId":       order.ID,
 				"orderedAt":     order.Ordered_at,
@@ -76,17 +80,20 @@ func (idb *InDB) CreateOrders(c *gin.Context) {
 			})
 		}
 	}
-
 }
 
 // get all data order
 func (idb *InDB) GetOrders(c *gin.Context) {
+
+	// init variabel
 	var (
 		orders []structs.Order
 		result gin.H
 	)
-	//
+	// find all orders
 	idb.DB.Find(&orders)
+
+	// if orders zero/0 response message
 	if len(orders) <= 0 {
 		result = gin.H{
 			"result":  nil,
@@ -95,6 +102,8 @@ func (idb *InDB) GetOrders(c *gin.Context) {
 		}
 		c.JSON(http.StatusNotFound, result)
 	} else {
+
+		// response get found order
 		result = gin.H{
 			"result": orders,
 			"count":  len(orders),
@@ -105,8 +114,11 @@ func (idb *InDB) GetOrders(c *gin.Context) {
 
 // update order by orderId
 func (idb *InDB) UpdateOrder(c *gin.Context) {
+
+	// req param :orderId
 	id := c.Param("orderId")
 
+	// struct data2
 	type Data2 struct {
 		LineItemId  int    `json:"lineItemId"`
 		ItemCode    string `json:"itemCode"`
@@ -114,6 +126,7 @@ func (idb *InDB) UpdateOrder(c *gin.Context) {
 		Quantity    int    `json:"quantity"`
 	}
 
+	// variable init
 	var (
 		order    structs.Order
 		item     structs.Item
@@ -124,19 +137,26 @@ func (idb *InDB) UpdateOrder(c *gin.Context) {
 	// check id order
 	err := idb.DB.Where("id = ?", id).First(&order).Error
 	if err != nil {
+
+		// response data order not found
 		result = gin.H{
 			"result": "data order not found",
 		}
 		c.JSON(http.StatusNotFound, result)
 	} else {
+
 		// check order_id on item
 		err = idb.DB.Where("order_id = ?", id).First(&item).Error
 		if err != nil {
+
+			// response data item not found
 			result = gin.H{
 				"result": "data item not found",
 			}
 			c.JSON(http.StatusNotFound, result)
+
 		} else {
+
 			// save data to new data variable
 			orderid, _ := strconv.Atoi(id)
 			customer_name := c.PostForm("customer_name")
@@ -152,17 +172,22 @@ func (idb *InDB) UpdateOrder(c *gin.Context) {
 				Description: item.Description,
 				Quantity:    item.Quantity,
 			}
+
 			// add object to array
 			data = append(data, dataStruct)
 
 			// update data
 			err = idb.DB.Model(&order).Updates(newOrder).Error
 			if err != nil {
+
+				// response update failed
 				result = gin.H{
 					"result": "Update Failed",
 				}
-				c.JSON(http.StatusBadRequest, result)
+				c.JSON(http.StatusNoContent, result)
 			} else {
+
+				// response update success
 				result = gin.H{
 					"result":        "successfully updated data",
 					"orderId":       order.ID,
@@ -179,41 +204,62 @@ func (idb *InDB) UpdateOrder(c *gin.Context) {
 
 // delete data with {id}
 func (idb *InDB) DeleteOrder(c *gin.Context) {
+
+	// init variable
 	var (
 		order  structs.Order
 		item   structs.Item
 		result gin.H
 	)
+
+	// param orderId
 	orderId := c.Param("orderId")
 
+	// querry check id on order
 	err := idb.DB.Where("id = ?", orderId).First(&order).Error
 	if err != nil {
+
+		// response data order not found
 		result = gin.H{
 			"result": "data order not found",
 		}
 		c.JSON(http.StatusNotFound, result)
 	} else {
+
+		// querry check order_id on item
 		err = idb.DB.Where("order_id = ?", orderId).First(&item).Error
 		if err != nil {
+
+			// response data item not found
 			result = gin.H{
 				"result": "data item not found",
 			}
 			c.JSON(http.StatusNotFound, result)
 		} else {
+
+			// Delete data on db, table item
 			err = idb.DB.Delete(&item).Error
 			if err != nil {
+
+				// response delete data item not success
 				result = gin.H{
 					"result": "Delete item Failed",
 				}
 				c.JSON(http.StatusBadRequest, result)
 			} else {
+
+				// Delete data on db, table order
 				err = idb.DB.Delete(&order).Error
 				if err != nil {
+
+					// response delete data order not success
 					result = gin.H{
 						"result": "Delete order Failed",
 					}
 					c.JSON(http.StatusBadRequest, result)
 				} else {
+
+					// request delete data success
 					result = gin.H{
 						"result": "data deleted successfully",
 						"data":   order,
@@ -223,5 +269,4 @@ func (idb *InDB) DeleteOrder(c *gin.Context) {
 			}
 		}
 	}
-
 }
